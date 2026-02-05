12a4874d85bc3b7b99b9863ae5d8aa3d56386015
codeAkhilesh14 <av384783@gmail.com>
2026-02-05 12:55:19 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -31,7 +31,7 @@ import SeniorAbout from './pages/SeniorAbout.jsx'
 import Contact from './components/ContactUs.jsx'
 import ConnectUs from './components/ConnectUs.jsx'
 
-export const serverUrl = "https://virtual-campus-backend.onrender.com"
+export const serverUrl = "https://virtual-campus-sx7r.onrender.com/"
 
 const App = () => {
   getCurrentUser();
